fdcfb256652f975312e8c9ac09d88842482be49b
mbarrientos.externos <mariobarrientos0303@gmail.com>
2022-12-14 12:28:11 -0500

    Primera carga Bot

--- bot.py
+++ bot.py
@@ -21,16 +21,20 @@ number_after = '61696'
 message_count = 0
 text_init = '_'
 
-
+# Inicializamos la conversación y respondemos con un saludo personalizado 
 def start(update, context:CallbackContext):
+    # Guardamos en memoria el nombre y creamos en memoria los números
     name = update.effective_user['first_name']
     context.user_data['number_before'] = '0'
     context.user_data['number_after'] = '61696'
+
+    # Respuesta
     text_init = f'Hola {name}, \n<b>Vamos a calcular en qué momento despega el cohete!!!...</b>\n<ins><b>Fíjate con atención en la siguientes imágenes y dime si en la que eliges el cohete ha despegado o no</b></ins>\n(<i>Un pequeño consejo sería que estés atento al contador de la parte superior derecha de la pantalla (T-): aún no ha despegado, (T+): ya despegó</i>)\nTe preguntaré y contestarás un <b>SI</b> o un <b>NO</b> para ir definiedo el rango donde se encuetra la imagen del despegue, <b>TENGO MÁXIMO 16 INTENTOS PARA LOGRARLO</b>\nSi tienes un muy buen ojo y tu rango se redujo al punto de no poder avanzar, puedes pulsar <b>CALCULAR</b> para que definamos con precisión en que imagen despegó el cohete\n¿Estás listo? /ready para empezar el juego.'
     update.message.reply_text(text=text_init , parse_mode=telegram.ParseMode.HTML)
     context.user_data['text_init'] = text_init
     return INPUT_NUMBER
 
+# Reinicio del Bot y Reinicio de caché
 def restart(update, context:CallbackContext):
     bot = telegram.Bot(token=TOKEN)
     chat_id = update.effective_user['id']
@@ -39,12 +43,15 @@ def restart(update, context:CallbackContext):
     bot.send_message(chat_id=chat_id, text=context.user_data['text_init'], parse_mode=telegram.ParseMode.HTML) 
     return INPUT_NUMBER
 
+# Pregunta Inicial
 def ready_command_handler(update, context:CallbackContext):
     clear_cahe(context)
     update.message.reply_text('Tenemos un video fraccionado en <strong>61696 imágenes</strong>, \nDime el número de la imagen que quieres tomar <ins><b>del 0 al 61696</b></ins> para poder calcular en cuál está despegando el cohete', parse_mode=telegram.ParseMode.HTML)
 
     return INPUT_NUMBER
 
+# Si la respuesta es /no acortamos la muestra desde momento inicial a momento sugerido por usuario
+# Guardamos en memoria el dato recibido
 def ready_command_handler_before(update, context:CallbackContext): 
     number_before = context.user_data.get('numero')
     context.user_data['number_before'] = number_before
@@ -55,6 +62,8 @@ def ready_command_handler_before(update, context:CallbackContext):
     update.message.reply_text('De acuerdo, intentemos con otro número entre ' + number_before + ' y ' + number_after+'\n Puedes reiniciar el juego con el comando /restart')
     return INPUT_NUMBER
 
+# Si la respuesta es /si acortamos la muestra desde momento final a momento sugerido por usuario
+# Guardamos en memoria el dato recibido
 def ready_command_handler_after(update, context:CallbackContext):
     number_after = context.user_data.get('numero')
     context.user_data['number_after'] = number_after
@@ -66,25 +75,8 @@ def ready_command_handler_after(update, context:CallbackContext):
     update.message.reply_text('De acuerdo, intentemos con otro número entre ' + number_before + ' y ' + number_after +'\n Puedes reiniciar el juego con el comando /restart')
     return INPUT_NUMBER
 
-def clear_cahe(context:CallbackContext):
-    context.user_data['number_before'] = '0'
-    context.user_data['number_after'] = '61696'
-    global message_count
-    message_count = 0
-
-def isNumeric(s):
-    return s.isdigit()
-
-def send_img(f, chat):
-    chat.send_action(
-        action=ChatAction.UPLOAD_PHOTO,
-        timeout=50000
-    )
-    chat.send_photo(
-        photo=open(f.name, 'rb')
-    )
-    os.unlink(f.name)
-
+# Si la respuesta es /calcular con la muestra reducida un promedio y pintamos el frame resultante 
+# Actualizamos caché
 def calcular(update, context:CallbackContext):
     resultado = round((int(context.user_data.get('number_before')) + int(context.user_data.get('number_after')))/2)
     response = requests.get('http://framex-dev.wadrid.net/api/video/Falcon%20Heavy%20Test%20Flight%20(Hosted%20Webcast)-wbSwFU6tY1c/frame/' + str(resultado), stream=True)
@@ -98,9 +90,11 @@ def calcular(update, context:CallbackContext):
     clear_cahe(context)
     return ConversationHandler.END
 
+
+# Empezamos la orden 
 def input_number(update, context:CallbackContext):
     number = update.message.text
-    
+    # Verificamos si el ingreso es un número válido y tomamos una decisión
     test = isNumeric(number)
     if number == '/ready':
             ready_command_handler(update, context)
@@ -115,6 +109,7 @@ def input_number(update, context:CallbackContext):
             update.message.reply_text(f'Debes ingresar un número mayor a {number}.\n Puedes reiniciar el juego con el comando /restart')
             return INPUT_NUMBER
     else:
+        # Si los campos son correctos empezamos la validación
         if test:
             global message_count
             message_count += 1
@@ -137,13 +132,35 @@ def input_number(update, context:CallbackContext):
         else:
             update.message.reply_text('Debes ingresar un número válido.\n Puedes reiniciar el juego con el comando /restart')
 
+# Funciones comunes para las instancias del Bot
+
+# Limpieza de memoria
+def clear_cahe(context:CallbackContext):
+    context.user_data['number_before'] = '0'
+    context.user_data['number_after'] = '61696'
+    global message_count
+    message_count = 0
+
+def isNumeric(s):
+    return s.isdigit()
+
+# Envío de imagen 
+def send_img(f, chat):
+    chat.send_action(
+        action=ChatAction.UPLOAD_PHOTO,
+        timeout=50000
+    )
+    chat.send_photo(
+        photo=open(f.name, 'rb')
+    )
+    os.unlink(f.name)
 
 if __name__ == '__main__':
-        
+    #Conexíon a Telegram Bot
     updater = Updater(token=TOKEN, use_context=True)
     dp = updater.dispatcher
 
-    # Add Handler
+    # Añadimos el  Handler
     dp.add_handler(CommandHandler('start', start))
     
     dp.add_handler(ConversationHandler(
@@ -159,6 +176,6 @@ if __name__ == '__main__':
         fallbacks=[]
     ))
 
-
+    # Escucha activa del chat
     updater.start_polling()
     updater.idle()
